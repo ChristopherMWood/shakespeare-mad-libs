18e0d9c0a2cc20c09c5977021804fa08e97eb91c
Christopher M. Wood <cmw2379@vt.edu>
2015-09-12 10:19:42 -0400

    Popup working among other small changes

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 $(function() {
 	$('#about-popup-anchor').magnificPopup({
 	  type:'inline',
-	  midClick: true // allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source.
+	  midClick: true,
+	  mainClass: 'mfp-fade'
 	});
 });
