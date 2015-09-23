9d2de5fb40f99cd08504c07b893fddd55d2dd941
Christopher M. Wood <cmw2379@vt.edu>
2015-09-23 15:21:14 -0400

    Major update to getting xml to render into html page

--- js/madlib_layout.js
+++ js/madlib_layout.js
@@ -1,44 +1,35 @@
 
 $(function() {
-  //$("#tab1-madlib-container").load("macbeth_lib.xml");
-  loadXMLDoc("macbeth_lib.xml");
-  //alert(loadXMLDoc("../macbeth_lib.xml"));
-  //displayResult('macbeth_lib.xml', 'macbeth_lib.xsl');
+  var xmlFilename = "./xml_resources/" + $("#xml-file-name").val();
+  var xslFileName = "./xml_resources/madlib_layout.xsl";
+  displayResult(xmlFilename, xslFileName);
 });
 
 function loadXMLDoc(filename)
 {
-  var fileResponse;
-  var request = $.ajax({
-      url: filename,
-      async: true
-  });
-
-  request.done(function(data){
-      fileResponse = data;
-  });
-
-  return fileResponse;
+if (window.ActiveXObject)
+  {
+  xhttp = new ActiveXObject("Msxml2.XMLHTTP");
+  }
+else 
+  {
+  xhttp = new XMLHttpRequest();
+  }
+xhttp.open("GET", filename, false);
+try {xhttp.responseType = "msxml-document"} catch(err) {} // Helping IE11
+xhttp.send("");
+return xhttp.responseXML;
 }
 
-function displayResult(xmlFilename, xslFilename)
+function displayResult(xmlFilename, xslFileName)
 {
-var xml = loadXMLDoc(xmlFilename);
-//var xsl = loadXMLDoc(xslFilename);
-// code for IE
-if (window.ActiveXObject || xhttp.responseType == "msxml-document")
-  {
-  ex = xml.transformNode(xsl);
-  document.getElementById("tab1-madlib-container").innerHTML = ex;
-  alert(ex);
-  }
-// code for Chrome, Firefox, Opera, etc.
-else if (document.implementation && document.implementation.createDocument)
+xml = loadXMLDoc(xmlFilename);
+xsl = loadXMLDoc(xslFileName);
+if (document.implementation && document.implementation.createDocument)
   {
   xsltProcessor = new XSLTProcessor();
   xsltProcessor.importStylesheet(xsl);
   resultDocument = xsltProcessor.transformToFragment(xml, document);
   document.getElementById("tab1-madlib-container").appendChild(resultDocument);
-  alert(resultDocument);
   }
 }
